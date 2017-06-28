418ae33598e2abe1da29934c2977f999d29c9be6
trolizmaslom <trolizmaslom@gmail.com>
2017-06-28 17:13:50 +0300

    day off

--- src/js/develop.js
+++ src/js/develop.js
@@ -4,6 +4,14 @@ function showMoreSkill() {
         $('.skill__item.hidden').removeClass('hidden');
     });
 }
+
 $(document).ready(function () {
     showMoreSkill();
+    $('.video__slider').slick({
+        centerMode: true,
+        infinity: false,
+        slidesToShow: 3,
+       arrows:false,
+        dots:true
+    });
 })
